fc7528598c11592fb27159cd869482ec586b77b7
Emerson Caldeira <87244779+Esc9919@users.noreply.github.com>
2023-07-02 13:24:49 -0300

    Pequena Alteração

--- API-Usuarios/src/main/java/br/com/usuarios/api/entidades/usuario/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package br.com.usuarios.api.entidades.usuario;
-
-
-
-                        public class HelloWorld {
-                            public static void main(String[] args) {
-                                System.out.println("Hello World");
-                            }
-                        }
